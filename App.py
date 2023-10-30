808d5092cbcdd6ddba847557f7b64e9356a2417f
Taborda-2010 <125746049+Taborda-2010@users.noreply.github.com>
2023-10-30 02:12:44 -0500

    Update App.py

--- App.py
+++ App.py
@@ -38,44 +38,32 @@ cf = TinyDB('cf.json')
 
 
             
+
 def promedio(receta_nombre, nueva_calificacion):
-    """
-    Calcula el promedio de calificaciones para una receta y agrega una nueva calificación.
-
-    Esta función busca las calificaciones existentes para una receta y calcula el promedio de
-    esas calificaciones. Si no hay calificaciones previas para la receta, agrega la nueva 
-    calificación directamente. Luego, muestra un mensaje con la calificación ingresada y el 
-    promedio actual.
-
-    Parameters:
-    receta_nombre (str): El nombre de la receta para la cual se va a calcular el promedio.
-    nueva_calificacion (float): La nueva calificación a agregar (de 1 a 5).
-
-    Returns:
-    float: El promedio actual de calificaciones para la receta.
-
-    Raises:
-    Exception: Si ocurre un error durante la ejecución.
-    """
     try:
         receta = Query()
-        # Filtrar busqueda por el título de la receta
-        calificaciones = [item['Título'] for item in cf.search(receta.Título == receta_nombre) if isinstance(item['Calificación'], (int, float))]
+        busqueda = cf.search(receta.Título == receta_nombre)
 
-        if not calificaciones:
+        if not busqueda:
+            # Si no se encuentra la receta, agrega la calificación directamente
             agregar_calificacion(receta_nombre, nueva_calificacion)
-            nueva_calificacion = round(nueva_calificacion,2)
-            imp = f'Tu calificación es {nueva_calificacion} y nuevaa {nueva_calificacion} '
+            imp = f'Tu calificación es {nueva_calificacion} y el promedio es {nueva_calificacion}'
             st.success(imp)
         else:
-            # Calcular el promedio solo con las calificaciones de la receta específica
-            promedio_calificaciones = round(sum(calificaciones) / len(calificaciones),2)
-            imp = f'Tu calificación es {nueva_calificacion} y el promedio de calificación de esta receta es {promedio_calificaciones} '
-            st.success(imp)
+            # Extraer las calificaciones válidas de la búsqueda
+            calificaciones = [item['Calificación'] for item in busqueda if isinstance(item['Calificación'], (int, float))]
 
+            # Verificar si hay calificaciones válidas antes de calcular el promedio
+            if calificaciones:
+                calificaciones.append(nueva_calificacion)  # Agregar la nueva calificación
+                promedio_calificaciones = sum(calificaciones) / len(calificaciones)
+                imp = f'Tu calificación es {nueva_calificacion} y el promedio de calificación de esta receta es {promedio_calificaciones}'
+                st.success(imp)
+            else:
+                st.warning("No hay calificaciones válidas para calcular el promedio.")
     except Exception as e:
         st.warning(f"Error en la función promedio: {e}")
-        return None
+
 
 
 
